79a58b2632e4b67cf936f492592b6638b5741695
Caelan Larsen <clars002@odu.edu>
2025-01-04 17:22:14 -0500

    working title screen, teensy optimizations

--- scenes/dungeoneer.gd
+++ scenes/dungeoneer.gd
@@ -9,22 +9,22 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var direction = Vector2.ZERO
+	var direction = Vector2i.ZERO
 	var move_vector
 	var destination
 	
 	if Input.is_action_just_pressed("ui_up"):
 		destination = $"../TileMapLayer".get_neighbor_cell(coordinates, 12)
-		move_vector = Vector2i(0, -1) # TODO: Optimize this to use pre-created vectors instead of making new every frame (dictionary/enum?)
+		move_vector = Vector2i.UP
 	elif Input.is_action_just_pressed("ui_right"):
 		destination = $"../TileMapLayer".get_neighbor_cell(coordinates, 0)
-		move_vector = Vector2i(1, 0)
+		move_vector = Vector2i.RIGHT
 	elif Input.is_action_just_pressed("ui_down"):
 		destination = $"../TileMapLayer".get_neighbor_cell(coordinates, 4)
-		move_vector = Vector2i(0, 1)
+		move_vector = Vector2i.DOWN
 	elif Input.is_action_just_pressed("ui_left"):
 		destination = $"../TileMapLayer".get_neighbor_cell(coordinates, 8)
-		move_vector = Vector2i(-1, 0)
+		move_vector = Vector2i.LEFT
 	
 	if move_vector:
 		var tile_data = $"../TileMapLayer".get_cell_tile_data(destination)
@@ -33,4 +33,4 @@ func _process(delta: float) -> void:
 		
 		if tile_type == 0:
 			coordinates += move_vector
-			position = 64 * coordinates
+			position = step_size * coordinates
